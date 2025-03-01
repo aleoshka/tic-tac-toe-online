37bb4047259133d709d2d3352288c9e1bedbb5ef
aleoshka <121514362+aleoshka@users.noreply.github.com>
2025-03-01 15:00:56 +0300

    feat: GAME-23 Added onClick to GameCell (#3)

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,3 +1,4 @@
 export { GameField } from "../Game/GameField";
 export { GameInfo } from "../Game/GameInfo";
 export { GameTitle } from "../Game/GameTitle";
+export { GameSymbol } from "../Game/GameSymbol";
